a89b4f3dea28bbf3c4c774ed0d11d963cb362970
printnewcode <zenit200803@mail.ru>
2024-05-07 19:31:11 +0300

    Test

--- base1.py
+++ base1.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sqla
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 
 engine = sqla.create_engine('sqlite:///base2.db', echo=True)
@@ -7,8 +8,8 @@ metadata = sqla.MetaData()
 Session = sessionmaker(autoflush=False, bind=engine)
 
 Users = sqla.Table('users', metadata,
-                   sqla.Column('id_user', sqla.Integer),
-                   sqla.Column('username', sqla.Text)
+                   sqla.Column('id_user', sqla.Integer, unique=True),
+                   sqla.Column('username', sqla.Text, unique=True)
                    )
 
 metadata.create_all(engine)
@@ -21,8 +22,10 @@ async def adding_data(username: str, user_id: int) -> None:
     # Не работает
     select_all_query = sqla.select(Users)
     select_all_results = conn.execute(select_all_query)
-    print()
-    if user_id not in select_all_results.fetchall() or username not in select_all_results.fetchall():
+    print(select_all_results.fetchall())
+    try:
+        user = Users.query.filter_by(username=username).one()
+    except NoResultFound:
         conn.execute(adding)
         conn.commit()
     else:
